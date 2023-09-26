52b828d3f7bd85dd4c9f868c89844f2596313a84
Nicholas Barooah <nicholas.barooah@dnata.com>
2023-09-26 13:35:21 +0100

    increased timeout

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  staticPageGenerationTimeout: 120,
+  staticPageGenerationTimeout: 1000,
   experimental: {
     appDir: true,
     serverActions: true
