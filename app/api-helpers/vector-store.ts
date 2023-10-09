cac3d3a446a9e4c264519d1f2dd82db17d30c402
Nicholas Barooah <nicholas.barooah@dnata.com>
2023-10-09 17:02:50 +0100

    caching embeddings

--- app/api-helpers/vector-store.ts
+++ app/api-helpers/vector-store.ts
@@ -1,17 +1,29 @@
 import { Document } from 'langchain/dist/document';
 import { VectorStoreRetriever } from 'langchain/dist/vectorstores/base';
+import { CacheBackedEmbeddings } from 'langchain/embeddings/cache_backed';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { InMemoryStore } from 'langchain/storage/in_memory';
 import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 
 export async function vectorStoreRetriever(
   docs: Document<Record<string, any>>[]
 ): Promise<VectorStoreRetriever<MemoryVectorStore>> {
-  const embeddings = new OpenAIEmbeddings();
+  const underlyingEmbeddings = new OpenAIEmbeddings();
+
+  const inMemoryStore = new InMemoryStore();
+
+  const cacheBackedEmbeddings = CacheBackedEmbeddings.fromBytesStore(
+    underlyingEmbeddings,
+    inMemoryStore,
+    {
+      namespace: underlyingEmbeddings.modelName
+    }
+  );
 
   if (docs) {
     console.log('creating vector store...');
     return (
-      await MemoryVectorStore.fromDocuments(docs, embeddings)
+      await MemoryVectorStore.fromDocuments(docs, cacheBackedEmbeddings)
     ).asRetriever();
   }
 
