66dc197235ac8bf876e371f1c82c67d79d84c569
barooahn <barooahn@gmail.com>
2023-10-20 12:07:00 +0100

    removed logo import

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,7 +2,7 @@ import { getSession } from '@/app/supabase-server';
 import AuthUI from './AuthUI';
 
 import { redirect } from 'next/navigation';
-import Logo from '@/components/icons/Logo';
+// import Logo from '@/components/icons/Logo';
 
 export default async function SignIn() {
   const session = await getSession();
@@ -15,7 +15,7 @@ export default async function SignIn() {
     <div className="flex justify-center height-screen-helper">
       <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
         <div className="flex justify-center pb-12 ">
-          <Logo width="64px" height="64px" />
+          {/* <Logo width="64px" height="64px" /> */}
         </div>
         <AuthUI />
       </div>
