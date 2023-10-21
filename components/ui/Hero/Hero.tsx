71725e0f9bc075c59043ce97738cc4e0ca9e36f3
Nicholas Ronald Barooah <barooahn@gmail.com>
2023-10-22 00:18:39 +0100

    less hero padding

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export function Hero() {
   return (
-    <div className="flex flex-col lg:flex-row sm:pl-2 lg:p-10 w-full">
+    <div className="flex flex-col lg:flex-row sm:pl-2 lg:px-10 w-full">
       <div className="block lg:hidden p-2 s:p-4">
         <Image priority height={40} src={Logo} alt="Chat Clause logo full" />
       </div>
